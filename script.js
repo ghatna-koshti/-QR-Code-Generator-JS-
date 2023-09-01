c1bc8cb0be80072d9f321ab29962db89da4c740a
ghatna-koshti <ghatnakoshti@gmail.com>
2023-09-01 11:42:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const qrText = document.getElementById("qr-text");
+const sizes = document.getElementById("sizes");
+const generateBtn = document.getElementById("generateBtn");
+const downloadBtn = document.getElementById("downloadBtn");
+const qrContainer = document.querySelector(".qr-body");
+
+generateBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  isEmptyInput()
+});
+
+downloadBtn.addEventListener("click",()=>{
+    let img = document.querySelector('.qr-body img');
+
+    if(img!=null){
+        let imgAtr = img.getAttribute('src');
+        downloadBtn.setAttribute("href",imgAtr);
+    }
+    else{
+        downloadBtn.setAttribute("href",`${document.querySelector('canvas').toDataURL()}`)
+    }
+
+})
+
+sizes.addEventListener("change", (e) => {
+  size = e.target.value;
+  isEmptyInput()
+});
+
+function isEmptyInput() {
+//   if (qrText.value.length > 0) {
+//     generateQRCode();
+//   } else {
+//     alert("Enter the text or URL to generate QR Code");
+//   }
+  (qrText.value.length > 0)?generateQRCode():alert("Enter the text or URL to generate QR Code");
+}
+
+let size = sizes.value;
+
+function generateQRCode() {
+  qrContainer.innerHTML = "";
+  new QRCode(qrContainer, {
+    text: qrText.value,
+    height: size,
+    width: size,
+    colorLight: "#fff",
+    colorDark: "#000",
+  });
+}
